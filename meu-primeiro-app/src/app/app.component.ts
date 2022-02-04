ab7bb23e544609dbb79f2aa931111eb555c9661b
Gustavo Lemos <gustavo.lemos@deal.com.br>
2022-02-04 08:25:14 -0300

    Seção 6 e 7 - Module, input e output

--- meu-primeiro-app/src/app/app.component.ts
+++ meu-primeiro-app/src/app/app.component.ts
@@ -3,12 +3,28 @@ import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit,
 @Component({
   selector: 'app-root',
   template: `
-    <app-diretivas-atributos></app-diretivas-atributos>
+    <ng-template [ngIf]="getDados">
+      <h1>{{getDados.nome}}</h1>
+      <h2>{{getDados.idade}}</h2>
+    </ng-template>
+    <app-output (enviarDados)="setDados($event)"></app-output>
     <router-outlet></router-outlet>`
 })
 export class AppComponent implements OnInit {
 
+  public addValue: number = 0;
+
+  public getDados: {nome: string, idade: number} | undefined;
+
   constructor() { }
 
   ngOnInit(): void {  }
+
+  public add() {
+    this.addValue += 1;
+  }
+
+  public setDados(event: any) {
+    this.getDados = event;
+  }
 }
