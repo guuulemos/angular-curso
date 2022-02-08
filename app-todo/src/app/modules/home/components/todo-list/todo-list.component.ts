0803dea366710a1f88ea4929ab47a12c82817711
Gustavo Lemos <83557907+guuulemos@users.noreply.github.com>
2022-02-08 08:08:25 -0300

    Criei o visual e alguns comportamentos

--- app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
+++ app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskList } from '../../model/task-list';
 
 @Component({
   selector: 'app-todo-list',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoListComponent implements OnInit {
 
+  public taskList: Array<TaskList> = [
+    { task: "Minha primeira Task", checked: true},
+    { task: "Minha segunda Task", checked: false}
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public deleteItemTaskList(event: number) {
+    this.taskList.splice(event, 1);
+  }
+
+  public deleteAllTaskList() {
+    const confirm = window.confirm("Você deseja realmente deletar tudo?")
+
+    if (confirm) {
+      this.taskList = [];
+    }
   }
 
 }
